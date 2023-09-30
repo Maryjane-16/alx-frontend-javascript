33dc827c38ba9ea43e0f6802300356e2264662ff
Maryjane-16 <okwuazichinazor@gmail.comwq>
2023-09-30 06:00:04 +0100

    export function

---
+++ 0x01-ES6_promise/9-try.js
@@ -0,0 +1,12 @@
+export default function guardrail(mathFunction) {
+    const stack = [];
+  
+    try {
+      stack.push(mathFunction());
+    } catch (err) {
+      stack.push(err.toString());
+    } finally {
+      stack.push('Guardrail was processed');
+    }
+    return stack;
+  }
