97984ecac6ffffe85b2e1b652154299637f69633
Maryjane-16 <okwuazichinazor@gmail.com>
2023-10-02 05:08:48 +0000

    Define a math function

--- 0x01-ES6_promise/9-try.js
+++ 0x01-ES6_promise/9-try.js
@@ -1,12 +1,12 @@
 export default function guardrail(mathFunction) {
-    const stack = [];
-  
-    try {
-      stack.push(mathFunction());
-    } catch (err) {
-      stack.push(err.toString());
-    } finally {
-      stack.push('Guardrail was processed');
-    }
-    return stack;
+  const stack = [];
+
+  try {
+    stack.push(mathFunction());
+  } catch (err) {
+    stack.push(err.toString());
+  } finally {
+    stack.push('Guardrail was processed');
   }
+  return stack;
+}
